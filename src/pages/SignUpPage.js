248c9096ea1b06582dd17b50c90d17388b7b7478
guddnboy <hung3018@naver.com>
2025-03-02 21:32:17 +0900

    feat: 회원가입 기능 추가

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,17 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ADDRESS } from '../constants/constants';
+import { useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
   const [userId, setuserId] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [checkPassword, setCheckPassword] = useState(false);
+  const [userName, setuserName] = useState('');
 
-  const handleSignup = () => {
-    if (password !== confirmPassword) {
-      alert('비밀번호가 일치하지 않습니다.');
-      return;
+  const navigate = useNavigate();
+
+  const handleSignup = async () => {
+    const response = await fetch(`${ADDRESS}/auth/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userid: userId,
+        password: password,
+        username: userName,
+      }),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+      navigate('/');
     }
   };
+
+  useEffect(() => {
+    if (password !== '' && password === confirmPassword) {
+      setCheckPassword(true);
+    } else {
+      setCheckPassword(false);
+    }
+  }, [password, confirmPassword]);
 
   return (
     <div className='flex items-center justify-center min-h-screen bg-gray-100'>
@@ -19,7 +45,7 @@ const SignupPage = () => {
         <h2 className='text-2xl font-bold text-center mb-4'>회원가입</h2>
         <input
           type='userId'
-          placeholder='사용할 아이디 (이메일)'
+          placeholder='사용할 아이디'
           value={userId}
           onChange={(e) => setuserId(e.target.value)}
           className='w-full p-2 mb-3 border rounded-md'
@@ -27,8 +53,8 @@ const SignupPage = () => {
         <input
           type='text'
           placeholder='사용할 닉네임'
-          value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
+          value={userName}
+          onChange={(e) => setuserName(e.target.value)}
           className='w-full p-2 mb-3 border rounded-md'
         />
         <input
@@ -36,18 +62,26 @@ const SignupPage = () => {
           placeholder='사용할 비밀번호'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className='w-full p-2 mb-3 border rounded-md'
+          className={`w-full p-2 mb-3 border rounded-md ${
+            !checkPassword ? 'border-red-500' : 'border-green-500'
+          }`}
         />
         <input
           type='password'
           placeholder='비밀번호 확인'
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
-          className='w-full p-2 mb-3 border rounded-md'
+          className={`w-full p-2 mb-3 border rounded-md ${
+            !checkPassword ? 'border-red-500' : 'border-green-500'
+          }`}
         />
+
         <button
           onClick={handleSignup}
-          className='w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600'
+          className={`w-full text-white p-2 rounded-md hover:bg-blue-600 ${
+            checkPassword ? 'bg-blue-500' : 'bg-gray-400 cursor-not-allowed'
+          }`}
+          disabled={!checkPassword}
         >
           회원가입
         </button>
