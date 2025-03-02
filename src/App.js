68b1f45f9ce60f93af026e0368bed4d10968c325
guddnboy <hung3018@naver.com>
2025-03-02 18:15:29 +0900

    feat: 로그인 페이지 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LoginPage from './LoginPage';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  return (
+    <div className='App'>
+      <LoginPage />
+    </div>
+  );
+};
 
 export default App;
